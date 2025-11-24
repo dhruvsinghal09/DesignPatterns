0e23fd0658e29be92c47bdf2fdb6d915019c6457
Dhruv Singhal <dhruvknitian@gmail.com>
2025-11-25 00:33:40 +0530

    Added Builder Pattern.

---
+++ src/main/java/CreationalPatterns/BuilderPattern/Main.java
@@ -0,0 +1,20 @@
+package main.java.CreationalPatterns.BuilderPattern;
+
+public class Main {
+
+    public static void main(String[] args){
+        Flat flat = new Flat.FlatBuilder("Dhruv",
+                "area",
+                "chapprauli banger",
+                "sector 168",
+                "Noida",
+                "Uttar Pradesh")
+                .setFacing("eastFacing")
+                .setHasBalcony(true)
+                .setHasStoreRoom(false)
+                .setHasParking(true)
+                .setLandmark("near paras seaons")
+                .build();
+        System.out.println(flat);
+    }
+}
